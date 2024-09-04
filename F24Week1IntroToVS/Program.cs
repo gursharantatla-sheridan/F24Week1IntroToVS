f67a7319f10a6d2e6a082a5d310dc1ad94f038e1
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-04 18:15:34 -0400

    Add project files.

---
+++ F24Week1IntroToVS/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace F24Week1IntroToVS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // area of a circle
+
+            Console.Write("\n\nEnter radius: ");
+            //double r = double.Parse(Console.ReadLine());
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double area = Math.PI * r * r;
+
+            Console.WriteLine("\nArea = " + area);
+        }
+    }
+}
